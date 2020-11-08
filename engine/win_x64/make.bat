d63e41b12878e30be8e2cba8e83ef1b7df5b2ac8
Hugo Rambure <rambure.hugo@hotmail.fr>
2020-11-09 00:29:13 +0100

    .

--- engine/win_x64/make.bat
+++ engine/win_x64/make.bat
@@ -6,7 +6,7 @@ echo "-------- Compilation"
 set P=%cd%\..
 
 mkdir %P%\win_x64\bin
-xcopy %P%\win_x64\data %P%\win_x64\bin /E /Q /I /D /Y
+xcopy %P%\data %P%\win_x64\bin\data /E /Q /I /D /Y
 xcopy %P%\externals\SDL2-2.0.10\x64\SDL2.dll %P%\win_x64\bin /Q /I /D /Y
 
 set PathOBJ=%P%\win_x64\bin\obj
@@ -32,11 +32,11 @@ set DebugFlags=/Z7
 
 pushd %P%\win_x64\bin
 
-cl 	%CompilerFlags%	^
-	/DLIB_STB /DPLATFORM_LAYER_SDL /DRENDERER_OPENGL3 ^
-	%IncludeGL3W% %IncludeSTB% %IncludeSDL% ^
-	%SourceApplication% %SourceGL3W% ^
-	/link %LinkSTB% %LinkSDL% %LinkWindows% ^
+cl  %CompilerFlags%                                     ^
+	/DLIB_STB /DPLATFORM_LAYER_SDL /DRENDERER_OPENGL3   ^
+	%IncludeGL3W% %IncludeSTB% %IncludeSDL%             ^
+	%SourceApplication% %SourceGL3W%                    ^
+	/link %LinkSTB% %LinkSDL% %LinkWindows%             ^
 	/SUBSYSTEM:CONSOLE
 
 popd
