5451af410c15aff2b478d3821d49710002103648
Hugo Rambure <rambure.hugo@hotmail.fr>
2020-11-10 16:24:14 +0100

    tweakable reloading

--- engine/source/engine/developper_tools.h
+++ engine/source/engine/developper_tools.h
@@ -153,8 +153,7 @@ namespace BEEWAX_INTERNAL{
 
     static char* DEV_tweakable_file_mapping = nullptr;
 
-    static DEV_Tweakable_Entry* DEV_get_new_or_existing_tweakable(const char* name, DEV_Tweakable_Type type, DEV_Tweakable_Value default_value){
-
+    static DEV_Tweakable_Entry* DEV_search_existing_tweakable(const char* name, DEV_Tweakable_Type type){
         // NOTE(hugo): try to find a tweakable with this name and type
         dchunkarena<DEV_Tweakable_Entry, DEV_tweakable_chunk_size>::chunk* current_chunk = BEEWAX_INTERNAL::DEV_tweakable_entries.head;
 
@@ -179,22 +178,31 @@ namespace BEEWAX_INTERNAL{
             current_chunk = current_chunk->next;
         }
 
-        // NOTE(hugo): no relevant tweakable was found ie make a new one
-        DEV_Tweakable_Entry* new_entry = DEV_tweakable_entries.get();
-
-        new_entry->name = name;
-        new_entry->type = type;
-        new_entry->value = default_value;
-
-        return new_entry;
+        return nullptr;
     }
 
-    static void DEV_import_tweakable_entries_from_file(const char* filename){
+    static DEV_Tweakable_Entry* DEV_get_new_or_existing_tweakable(const char* name, DEV_Tweakable_Type type, DEV_Tweakable_Value default_value){
 
-        // NOTE(hugo): read entire file
-        FILE* file = fopen(filename, "r");
+        DEV_Tweakable_Entry* entry = DEV_search_existing_tweakable(name, type);
+
+        // NOTE(hugo): no relevant tweakable was found ie make a new one
+        if(!entry){
+            entry = DEV_tweakable_entries.get();
+
+            entry->name = name;
+            entry->type = type;
+            entry->value = default_value;
+        }
+
+        return entry;
+    }
+
+    static void DEV_read_entire_file(const char* filename, char*& out_data, size_t& out_bytesize){
+        FILE* file = fopen(filename, "rb");
         if(file == NULL){
-            LOG_ERROR("DEV_import_tweakable_entries_from_file(%s) FAILED - aborting import", filename);
+            LOG_ERROR("DEV_read_entire_file(%s) FAILED - aborting", filename);
+            out_data = nullptr;
+            out_bytesize = 0u;
             return;
         }
 
@@ -203,79 +211,123 @@ namespace BEEWAX_INTERNAL{
         s64 end_index = ftell(file);
         fseek(file, start_index, SEEK_SET);
 
-        s64 file_size = end_index - start_index;
-        if(file_size == 0u){
-            LOG_ERROR("tweakable file %s is empty", filename);
+        s64 file_bytesize = end_index - start_index;
+        if(file_bytesize == 0u){
+            LOG_INFO("DEV_read_entire_file(%s) - file was empty", filename);
+            out_data = nullptr;
+            out_bytesize = 0u;
             return;
         }
 
-        DEV_tweakable_file_mapping = (char*)malloc(file_size);
-        fread(DEV_tweakable_file_mapping, 1u, file_size, file);
-
+        char* file_data = (char*)malloc(file_bytesize);
+        fread(file_data, 1u, file_bytesize, file);
         fclose(file);
 
-        // NOTE(hugo): parse entries
-        char* cursor = DEV_tweakable_file_mapping;
-        while((cursor - DEV_tweakable_file_mapping) < file_size){
-            DEV_Tweakable_Entry* entry = DEV_tweakable_entries.get();
+        out_data = file_data;
+        out_bytesize = file_bytesize;
+    }
 
-            // NOTE(hugo): name
-            entry->name = cursor;
+    static DEV_Tweakable_Entry DEV_parse_tweakable_entry_at_cursor(char*& cursor){
+        DEV_Tweakable_Entry entry;
+
+        // NOTE(hugo): name
+        entry.name = cursor;
+        while(*cursor != ' ') ++cursor;
+        *cursor = '\0';
+        ++cursor;
+
+        // NOTE(hugo): type and value
+        if(memcmp(cursor, "BOOLEAN", 7u) == 0){
+            entry.type = TWEAKABLE_BOOLEAN;
             while(*cursor != ' ') ++cursor;
-            *cursor = '\0';
             ++cursor;
 
-            // NOTE(hugo): type and value
-            if(memcmp(cursor, "BOOLEAN", 7u) == 0){
-                entry->type = TWEAKABLE_BOOLEAN;
-                while(*cursor != ' ') ++cursor;
-                ++cursor;
-
-                if(memcmp(cursor, "true", 4u) == 0){
-                    entry->value.as_BOOLEAN = true;
-                }else{
-                    entry->value.as_BOOLEAN = false;
-                }
-                while(*cursor != '\n' && *cursor != EOF) ++cursor;
-
-            }else if(memcmp(cursor, "INTEGER", 7u) == 0){
-                entry->type = TWEAKABLE_INTEGER;
-                while(*cursor != ' ') ++cursor;
-                ++cursor;
-
-                sscanf(cursor, "%d", &entry->value.as_INTEGER);
-                while(*cursor != '\n' && *cursor != EOF) ++cursor;
-
-            }else if(memcmp(cursor, "REAL", 4u) == 0){
-                entry->type = TWEAKABLE_REAL;
-                while(*cursor != ' ') ++cursor;
-                ++cursor;
-
-                sscanf(cursor, "%f", &entry->value.as_REAL);
-                while(*cursor != '\n' && *cursor != EOF) ++cursor;
-
-            }else if(memcmp(cursor, "STRING", 6u) == 0){
-                entry->type = TWEAKABLE_STRING;
-                while(*cursor != ' ') ++cursor;
-                ++cursor;
-
-                entry->value.as_STRING = cursor;
-                while(*cursor != '\n' && *cursor != EOF) ++cursor;
-                *cursor = '\0';
-
+            if(memcmp(cursor, "true", 4u) == 0){
+                entry.value.as_BOOLEAN = true;
             }else{
-                LOG_ERROR("unknown type in tweakable file %s %.*s", filename, cursor, 10u);
-                assert(false);
-                return;
+                entry.value.as_BOOLEAN = false;
             }
+            while(*cursor != '\n' && *cursor != EOF) ++cursor;
+
+        }else if(memcmp(cursor, "INTEGER", 7u) == 0){
+            entry.type = TWEAKABLE_INTEGER;
+            while(*cursor != ' ') ++cursor;
             ++cursor;
+
+            sscanf(cursor, "%d", &entry.value.as_INTEGER);
+            while(*cursor != '\n' && *cursor != EOF) ++cursor;
+
+        }else if(memcmp(cursor, "REAL", 4u) == 0){
+            entry.type = TWEAKABLE_REAL;
+            while(*cursor != ' ') ++cursor;
+            ++cursor;
+
+            sscanf(cursor, "%f", &entry.value.as_REAL);
+            while(*cursor != '\n' && *cursor != EOF) ++cursor;
+
+        }else if(memcmp(cursor, "STRING", 6u) == 0){
+            entry.type = TWEAKABLE_STRING;
+            while(*cursor != ' ') ++cursor;
+            ++cursor;
+
+            entry.value.as_STRING = cursor;
+            while(*cursor != '\n' && *cursor != EOF) ++cursor;
+            *cursor = '\0';
+
+        }else{
+            LOG_ERROR("DEV_parse_tweakable_entry_at_cursor - unknown type starting as %.*s", cursor, 4u);
         }
+        ++cursor;
+        return entry;
+    }
+
+    static void DEV_import_tweakable_entries_from_file(const char* filename){
+
+        // NOTE(hugo): read entire file
+        size_t tweakable_file_bytesize;
+        DEV_read_entire_file(filename, DEV_tweakable_file_mapping, tweakable_file_bytesize);
+        if(!DEV_tweakable_file_mapping){
+            return;
+        }
+
+        char* cursor = DEV_tweakable_file_mapping;
+        while((cursor - DEV_tweakable_file_mapping) < tweakable_file_bytesize){
+            DEV_Tweakable_Entry* entry = DEV_tweakable_entries.get();
+            *entry = DEV_parse_tweakable_entry_at_cursor(cursor);
+        }
+    }
+
+    void DEV_reload_tweakable_entries_from_file(const char* filename){
+        LOG_INFO("DEV_reload_tweakable_entries_from_file()");
+
+        // NOTE(hugo): read entire file
+        char* new_file_mapping;
+        size_t tweakable_file_bytesize;
+        DEV_read_entire_file(filename, new_file_mapping, tweakable_file_bytesize);
+        if(!new_file_mapping){
+            return;
+        }
+
+        char* cursor = new_file_mapping;
+        while((cursor - new_file_mapping) < tweakable_file_bytesize){
+            DEV_Tweakable_Entry parsed_entry = DEV_parse_tweakable_entry_at_cursor(cursor);
+            DEV_Tweakable_Entry* entry = DEV_search_existing_tweakable(parsed_entry.name, parsed_entry.type);
+            if(!entry){
+                entry = DEV_tweakable_entries.get();
+            }
+            entry->name = parsed_entry.name;
+            entry->type = parsed_entry.type;
+            entry->value = parsed_entry.value;
+        }
+
+        free(DEV_tweakable_file_mapping);
+        DEV_tweakable_file_mapping = new_file_mapping;
     }
 
     static void DEV_export_tweakable_entries_to_file(const char* filename){
         dchunkarena<DEV_Tweakable_Entry, DEV_tweakable_chunk_size>::chunk* current_chunk = BEEWAX_INTERNAL::DEV_tweakable_entries.head;
 
-        FILE* file = fopen(filename, "w");
+        FILE* file = fopen(filename, "wb");
         if(file == NULL){
             LOG_ERROR("DEV_export_tweakable_entries_to_file(%s) FAILED - aborting export", filename);
             return;
@@ -384,5 +436,6 @@ namespace BEEWAX_INTERNAL{
 }()
 
 #define DEV_DISPLAY_TWEAKABLE_ENTRIES do{ BEEWAX_INTERNAL::DEV_LOG_tweakable_entries(); }while(0)
+#define DEV_RELOAD_TWEAKABLE_ENTRIES do{ BEEWAX_INTERNAL::DEV_reload_tweakable_entries_from_file("./data/tweakable.txt"); }while(0)
 
 #endif
