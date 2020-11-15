8a5a62226cc6a3510e430bfd75a3618b945f8600
Hugo Rambure <rambure.hugo@hotmail.fr>
2020-11-15 15:14:40 +0100

    improve DEFER

--- engine/source/engine/defer_macro.h
+++ engine/source/engine/defer_macro.h
@@ -4,27 +4,14 @@
 // NOTE(hugo): DEFER is optimized away when using optimizations (assembly checked for gcc & msvc)
 //             copying /action/ outputs smaller assembly than by ref. without optimizations
 
-struct DEFER_Creator{
-};
-
-template<typename DEFER_Action>
-struct DEFER_Temporary{
-    DEFER_Action action;
-};
-
 template<typename DEFER_Action>
 struct DEFER_Container{
-    DEFER_Container(DEFER_Temporary<DEFER_Action> temp) : action(temp.action){}
+    DEFER_Container(DEFER_Action&& input_action) : action(input_action){}
     ~DEFER_Container(){
         action();
     }
 
     DEFER_Action action;
-};
-
-template<typename DEFER_Action>
-DEFER_Temporary<DEFER_Action> operator+(DEFER_Creator, DEFER_Action&& action){
-    return DEFER_Temporary<DEFER_Action>{action};
 };
 
 // NOTE(hugo): capture by copy and not reference because variable values can change between DEFER and the execution
@@ -35,7 +22,7 @@ DEFER_Temporary<DEFER_Action> operator+(DEFER_Creator, DEFER_Action&& action){
 //
 //     ptr = nullptr;
 // }
-#define DEFER DEFER_Container CONCATENATE(DEFER_variable_at_, __LINE__) = DEFER_Creator() + [=]() mutable
+#define DEFER DEFER_Container CONCATENATE(DEFER_variable_at_, __LINE__) = [=]() mutable
 
 #else
 
