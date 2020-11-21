84228eebc72886bec6fdfb9db2d2521ba4e2336a
Hugo Rambure <rambure.hugo@hotmail.fr>
2020-11-21 15:25:12 +0100

    improve audio implementation

--- engine/source/engine/audio_SDL.cpp
+++ engine/source/engine/audio_SDL.cpp
@@ -1,3 +1,257 @@
+// NOTE(hugo): conversion from decibels to volume percents
+float dB_to_volume(float decibel){
+    return powf(10.f, 0.05f * decibel);
+}
+float volume_to_dB(float volume){
+    return 20.f * log10f(volume);
+}
+
+// ---- audio manager
+
+static void audio_callback(void* user_ptr, u8* out_stream, s32 out_stream_size);
+
+void Audio_Manager::setup(){
+    SDL_AudioSpec required_spec;
+    required_spec.freq = 44100u;
+    required_spec.format = AUDIO_S16;
+    required_spec.channels = 1u;
+    required_spec.samples = audio_device_buffer_in_samples;
+    required_spec.callback = audio_callback;
+    required_spec.userdata = (void*)&state;
+
+    SDL_AudioSpec returned_spec;
+    device = SDL_OpenAudioDevice(nullptr, 0, &required_spec, &returned_spec, 0);
+    if(!device){
+        LOG_ERROR("SDL_OpenAudioDevice() FAILED - %s", SDL_GetError());
+    }
+    device_spec = returned_spec;
+
+    state.nsamples = audio_manager_buffer_in_samples;
+    state.buffer = (s16*)malloc(sizeof(s16) * state.nsamples);
+    if(!state.buffer){
+        LOG_ERROR("malloc FAILED");
+    }
+
+    mix_buffer = (float*)malloc(sizeof(float) * audio_manager_generator_offset_in_samples);
+    if(!mix_buffer){
+        LOG_ERROR("malloc FAILED");
+    }
+
+    SDL_PauseAudioDevice(device, 0);
+}
+
+void Audio_Manager::terminate(){
+    SDL_PauseAudioDevice(device, 1);
+
+    buffers.free();
+    to_play.free();
+
+    SDL_CloseAudioDevice(device);
+}
+
+Audio_Buffer_ID Audio_Manager::buffer_from_wav(const File_Path& path){
+    SDL_AudioSpec wav_spec;
+    u8* wav_buffer;
+    u32 wav_bytesize;
+    if(!SDL_LoadWAV(path.data, &wav_spec, &wav_buffer, &wav_bytesize)){
+        LOG_ERROR("SDL_LoadWav() FAILED - %s", SDL_GetError());
+    }
+
+    // NOTE(hugo): convert to the device specification when needed
+    SDL_AudioCVT converter;
+    s32 converter_return_code = SDL_BuildAudioCVT(&converter,
+            wav_spec.format, wav_spec.channels, wav_spec.freq,
+            device_spec.format, device_spec.channels, device_spec.freq);
+    if(converter_return_code < 0){
+        LOG_ERROR("SDL_BuildAudioCVT() FAILED - %s", SDL_GetError());
+    }
+
+    Buffer_Data buffer;
+
+    // NOTE(hugo): wav_spec is usable by the device_spec
+    if(converter_return_code == 0){
+        buffer.data = (s16*)malloc(wav_bytesize);
+        buffer.nsamples = wav_bytesize / sizeof(s16);
+
+        memcpy(buffer.data, wav_buffer, wav_bytesize);
+        SDL_FreeWAV(wav_buffer);
+
+    // NOTE(hugo): conversion is required
+    }else if(converter_return_code == 1){
+        converter.len = wav_bytesize;
+        converter.buf = (u8*)malloc(converter.len * converter.len_mult);
+
+        memcpy(converter.buf, wav_buffer, wav_bytesize);
+        SDL_FreeWAV(wav_buffer);
+
+        if(SDL_ConvertAudio(&converter)){
+            LOG_ERROR("SDL_ConvertAudio() FAILED - %s", SDL_GetError());
+        }
+
+        buffer.data = (s16*)converter.buf;
+        buffer.nsamples = converter.len_cvt / sizeof(s16);
+    }
+
+    return buffers.insert(buffer);
+}
+
+void Audio_Manager::remove_buffer(Audio_Buffer_ID buffer){
+    Buffer_Data& buffer_entry = buffers[buffer];
+
+    u32 index, counter;
+    for(index = to_play.get_first(), counter = 0u;
+        index < to_play.capacity && counter != to_play.size;
+        index = to_play.get_next(index), ++counter){
+
+        if(to_play[index].buffer == buffer){
+            to_play.remove(index);
+            --counter;
+        }
+    }
+    ::free(buffer_entry.data);
+    buffers.remove(buffer);
+}
+
+Audio_Playing_ID Audio_Manager::start_playing(Audio_Buffer_ID buffer){
+    Play_Data play_data;
+    play_data.buffer = buffer;
+    play_data.cursor = 0u;
+    play_data.generation = manager_generation;
+
+    u32 index = to_play.size;
+    to_play.insert(play_data);
+    u32 generation = manager_generation;
+
+    ++manager_generation;
+
+    return {index, generation};
+}
+
+void Audio_Manager::stop_playing(Audio_Playing_ID play){
+    if(to_play.is_active(play.index) && to_play[play.index].generation == play.generation){
+        to_play.remove(play.index);
+    }
+}
+
+void Audio_Manager::mix_next_frame(){
+    u32 reader_cursor = atomic_get(&state.reader_cursor);
+    u32 generator_cursor = state.generator_cursor;
+
+    // NOTE(hugo): compute the number of samples to mix and the copy ranges to the staging buffer
+    u32 samples_mixed;
+    u32 samples_to_mix;
+    u32 first_start = generator_cursor;
+    u32 first_samples;
+    u32 second_start = 0u;
+    u32 second_samples;
+    if(generator_cursor >= reader_cursor){
+        samples_mixed = generator_cursor - reader_cursor;
+        samples_to_mix = audio_manager_generator_offset_in_samples - samples_mixed;
+        first_samples = min(samples_to_mix, state.nsamples - generator_cursor);
+        second_samples = samples_to_mix - first_samples;
+
+    // NOTE(hugo): generator_cursor < reader_cursor
+    }else{
+        samples_mixed = state.nsamples - reader_cursor + generator_cursor;
+        samples_to_mix = audio_manager_generator_offset_in_samples - samples_mixed;
+        first_samples = samples_to_mix;
+        second_samples = 0u;
+    }
+
+    u32 total_samples = first_samples + second_samples;
+
+    // NOTE(hugo): zero the mix buffer
+    for(u32 isample = 0u; isample != total_samples; ++isample){
+        mix_buffer[isample] = 0.f;
+    }
+
+    // NOTE(hugo): mix the samples
+    u32 index, counter;
+    for(index = to_play.get_first(), counter = 0u;
+            index < to_play.capacity;
+            index = to_play.get_next(index), ++counter){
+
+        Play_Data& play = to_play[index];
+        Buffer_Data& buffer = buffers[play.buffer];
+
+        u32 mix_sample = 0u;
+        u32 play_cursor = play.cursor;
+        while(mix_sample != total_samples && play_cursor != buffer.nsamples){
+            mix_buffer[mix_sample] += (float)buffer.data[play_cursor];
+            ++mix_sample;
+            ++play_cursor;
+        }
+        if(play_cursor == buffer.nsamples){
+            LOG_TRACE("removing");
+            to_play.remove(index);
+            --counter;
+        }else{
+            play.cursor = play_cursor;
+        }
+    }
+
+    // NOTE(hugo): convert to s16 and copy to the ring buffer
+    for(u32 isample = 0u; isample != first_samples; ++isample){
+        s16 sample = (s16)clamp<float>(mix_buffer[isample], SHRT_MIN, SHRT_MAX);
+        state.buffer[first_start + isample] = sample;
+    }
+    for(u32 isample = 0u; isample != second_samples; ++isample){
+        s16 sample = (s16)clamp<float>(mix_buffer[first_samples + isample], SHRT_MIN, SHRT_MAX);
+        state.buffer[second_start + isample] = sample;
+    }
+
+    // NOTE(hugo): update the generator cursor for the audio callback
+    u32 new_generator_cursor = (reader_cursor + audio_manager_generator_offset_in_samples) % state.nsamples;
+    atomic_set(&state.generator_cursor, new_generator_cursor);
+}
+
+static void audio_callback(void* user_ptr, u8* out_stream, s32 out_stream_size){
+    Audio_Manager::Audio_State* state = (Audio_Manager::Audio_State*)user_ptr;
+
+    u32 reader_cursor = state->reader_cursor;
+    u32 generator_cursor = atomic_get(&state->generator_cursor);
+    if(reader_cursor == generator_cursor){
+        memset(out_stream, 0, out_stream_size);
+        return;
+    }
+
+    u32 out_nsamples = (u32)out_stream_size / sizeof(s16);
+    u32 end_samples;
+    u32 start_samples;
+    if(generator_cursor > reader_cursor){
+        end_samples = generator_cursor - reader_cursor;
+        start_samples = 0u;
+    }else{
+        end_samples = state->nsamples - reader_cursor;
+        start_samples = generator_cursor;
+    }
+
+    // NOTE(hugo): compute the memcpy ranges from the staging buffer
+    u32 first_start = reader_cursor;
+    u32 first_samples = min(out_nsamples, end_samples);
+
+    u32 second_start = 0u;
+    u32 second_samples = min(out_nsamples - first_samples, start_samples);
+
+    u32 copy_samples = first_samples + second_samples;
+    u32 zero_samples = out_nsamples - copy_samples;
+
+    s16* out_samples = (s16*)out_stream;
+    memcpy(out_samples, state->buffer + first_start, first_samples * sizeof(s16));
+    memcpy(out_samples + first_samples, state->buffer + second_start, second_samples * sizeof(s16));
+    memset(out_samples + copy_samples, 0, zero_samples * sizeof(s16));
+
+    if(zero_samples){
+        LOG_TRACE("audio starvation; zero: %d", zero_samples);
+    }
+
+    u32 new_reader_cursor = reader_cursor + first_samples;
+    if(new_reader_cursor == state->nsamples){
+        new_reader_cursor = second_samples;
+    }
+    atomic_set<u32>(&state->reader_cursor, new_reader_cursor);
+}
+
 // ---- hardware / software detection
 
 void audio_detect_devices(){
--- engine/source/engine/audio_SDL.h
+++ engine/source/engine/audio_SDL.h
@@ -1,10 +1,11 @@
-static void audio_mix_samples_s16(s16* destination, s16* source, u32 nsamples){
-    for(u32 isample = 0u; isample != nsamples; ++isample){
-        destination[isample] += source[isample];
-    }
-}
+#ifndef H_AUDIO
+#define H_AUDIO
 
-static void audio_callback(void* user_ptr, u8* out_stream, s32 out_stream_size);
+// NOTE(hugo): 735 samples per frame per channel at 60 fps with a frequency of 44100Hz
+constexpr u32 audio_device_buffer_in_samples = 512u;
+constexpr u32 audio_manager_buffer_in_samples = 4096u;
+// NOTE(hugo): generator should be at least one frame ahead of reader at all times
+constexpr u32 audio_manager_generator_offset_in_samples = audio_manager_buffer_in_samples - 1u;
 
 typedef u32 Audio_Buffer_ID;
 struct Audio_Playing_ID{
@@ -12,214 +13,26 @@ struct Audio_Playing_ID{
     u64 generation;
 };
 
-// NOTE(hugo): 735 samples per frame per channel at 60 fps with a frequency of 44100Hz
-constexpr u32 audio_device_buffer_in_samples = 256u;
-constexpr u32 audio_manager_buffer_in_samples = 4096u;
-constexpr u32 audio_manager_generator_offset_in_samples = audio_manager_buffer_in_samples - 1u;
-
 struct Audio_Manager{
-    void setup(){
-        SDL_AudioSpec required_spec;
-        required_spec.freq = 44100u;
-        required_spec.format = AUDIO_S16;
-        required_spec.channels = 1u;
-        required_spec.samples = audio_device_buffer_in_samples;
-        required_spec.callback = audio_callback;
-        required_spec.userdata = (void*)&callback_data;
+    void setup();
+    void terminate();
 
-        SDL_AudioSpec returned_spec;
-        device = SDL_OpenAudioDevice(nullptr, 0, &required_spec, &returned_spec, 0);
-        if(!device){
-            LOG_ERROR("SDL_OpenAudioDevice() FAILED - %s", SDL_GetError());
-        }
+    Audio_Buffer_ID buffer_from_wav(const File_Path& path);
+    void remove_buffer(Audio_Buffer_ID buffer);
 
-        device_spec = returned_spec;
-        LOG_INFO("audio device specification:");
-        LOG_INFO("- frequency : %i", device_spec.freq);
-        LOG_INFO("- format: %s", (device_spec.format == AUDIO_S16) ? "device_s16" : "unknown");
-        LOG_INFO("- channels: %i", device_spec.channels);
-        LOG_INFO("- silence: %d", device_spec.silence);
-        LOG_INFO("- samples: %i", device_spec.samples);
-        LOG_INFO("- callback: %p", device_spec.callback);
-        LOG_INFO("- userdata: %p", device_spec.userdata);
+    Audio_Playing_ID start_playing(Audio_Buffer_ID buffer);
+    void stop_playing(Audio_Playing_ID play);
 
-        callback_data.nsamples = audio_manager_buffer_in_samples;
-        callback_data.buffer = (u8*)malloc(sizeof(s16) * callback_data.nsamples);
-        if(!callback_data.buffer){
-            LOG_ERROR("malloc FAILED - keeping the previous memory");
-        }
-
-        SDL_PauseAudioDevice(device, 0);
-    }
-
-    void terminate(){
-        // NOTE(hugo): free loaded buffers
-        for(u32 ibuffer = buffer_pool.get_first();
-            ibuffer < buffer_pool.capacity;
-            ibuffer = buffer_pool.get_next(ibuffer)){
-
-            unload_buffer(ibuffer);
-        }
-        buffer_pool.free();
-        play_pool.free();
-
-        SDL_CloseAudioDevice(device);
-    }
-
-    void next_frame(){
-        size_t sample_bytesize = sizeof(s16);
-
-        u32 reader_cursor = atomic_get(&callback_data.reader_cursor);
-        u32 generator_cursor = callback_data.generator_cursor;
-        u32 expected_generator_cursor = (reader_cursor + audio_manager_generator_offset_in_samples);
-        if(expected_generator_cursor >= callback_data.nsamples){
-            expected_generator_cursor -= callback_data.nsamples;
-        }
-
-        // NOTE(hugo): single memcpy
-        if(expected_generator_cursor > generator_cursor){
-            u32 samples_to_write = expected_generator_cursor - generator_cursor;
-            u8* write_ptr = callback_data.buffer + generator_cursor * sample_bytesize;
-            memset(write_ptr, 0, samples_to_write * sample_bytesize);
-
-            for(u32 iplay = play_pool.get_first();
-                    iplay < play_pool.capacity;
-                    iplay = play_pool.get_next(iplay)){
-
-                Play_Data& play_data = play_pool[iplay];
-                Buffer_Data& buffer = buffer_pool[play_data.buffer];
-
-                if(play_data.cursor + samples_to_write < buffer.nsamples){
-                    audio_mix_samples_s16((s16*)write_ptr, (s16*)(buffer.data + play_data.cursor * sample_bytesize), samples_to_write);
-                    //memcpy(write_ptr, buffer.data + play_data.cursor * sample_bytesize, samples_to_write * sample_bytesize);
-                    play_data.cursor += samples_to_write;
-
-                }else{
-                    u32 samples_to_copy = buffer.nsamples - play_data.cursor;
-                    audio_mix_samples_s16((s16*)write_ptr, (s16*)(buffer.data + play_data.cursor * sample_bytesize), samples_to_copy);
-                    //memcpy(write_ptr, buffer.data + play_data.cursor * sample_bytesize, samples_to_copy * sample_bytesize);
-                    play_pool.remove(iplay);
-                }
-            }
-
-        // NOTE(hugo): two memcpy and wrap around
-        }else if(expected_generator_cursor < generator_cursor){
-            u32 samples_to_write_end = callback_data.nsamples - generator_cursor;
-            u8* write_end = callback_data.buffer + generator_cursor * sample_bytesize;
-            memset(write_end, 0, samples_to_write_end * sample_bytesize);
-
-            u32 samples_to_write_start = expected_generator_cursor;
-            u8* write_start = callback_data.buffer;
-            memset(write_start, 0, samples_to_write_start * sample_bytesize);
-
-            u32 samples_to_write = samples_to_write_end + samples_to_write_start;
-
-            for(u32 iplay = play_pool.get_first();
-                    iplay < play_pool.capacity;
-                    iplay = play_pool.get_next(iplay)){
-
-                Play_Data& play_data = play_pool[iplay];
-                Buffer_Data& buffer = buffer_pool[play_data.buffer];
-
-                if(play_data.cursor + samples_to_write < buffer.nsamples){
-                    audio_mix_samples_s16((s16*)write_end, (s16*)(buffer.data + play_data.cursor * sample_bytesize), samples_to_write_end);
-                    audio_mix_samples_s16((s16*)write_start, (s16*)(buffer.data + (play_data.cursor + samples_to_write_end) * sample_bytesize), samples_to_write_start);
-                    //memcpy(write_end, buffer.data + play_data.cursor * sample_bytesize, samples_to_write_end * sample_bytesize);
-                    //memcpy(write_start, buffer.data + (play_data.cursor + samples_to_write_end) * sample_bytesize, samples_to_write_start * sample_bytesize);
-                    play_data.cursor += samples_to_write;
-
-                }else{
-                    u32 samples_to_copy = buffer.nsamples - play_data.cursor;
-                    audio_mix_samples_s16((s16*)write_end, (s16*)(buffer.data + play_data.cursor * sample_bytesize), min(samples_to_copy, samples_to_write_end));
-                    //memcpy(write_end, buffer.data + play_data.cursor * sample_bytesize, min(samples_to_copy, samples_to_write_end) * sample_bytesize);
-                    if(samples_to_copy > samples_to_write_end){
-                        audio_mix_samples_s16((s16*)write_start, (s16*)(buffer.data + (play_data.cursor + samples_to_write_end) * sample_bytesize), (samples_to_copy - samples_to_write_end));
-                        //memcpy(write_start, buffer.data + (play_data.cursor + samples_to_write_end) * sample_bytesize, (samples_to_copy - samples_to_write_end) * sample_bytesize);
-                    }
-                    play_pool.remove(iplay);
-                }
-            }
-
-        }
-
-        atomic_set(&callback_data.generator_cursor, expected_generator_cursor);
-    }
-
-    Audio_Buffer_ID load_wav(const File_Path& path){
-        SDL_AudioSpec wav_spec;
-        u8* wav_buffer;
-        u32 wav_bytesize;
-        if(!SDL_LoadWAV(path.data, &wav_spec, &wav_buffer, &wav_bytesize)){
-            LOG_ERROR("SDL_LoadWav() FAILED - %s", SDL_GetError());
-        }
-
-        LOG_INFO("wav specification");
-        LOG_INFO("- frequency : %i", wav_spec.freq);
-        LOG_INFO("- format: %s", (wav_spec.format == AUDIO_S16) ? "audio_s16" : "unknown");
-        LOG_INFO("- channels: %i", wav_spec.format);
-
-        // NOTE(hugo): convert to the device specification when needed
-        SDL_AudioCVT converter;
-        s32 converter_return_code = SDL_BuildAudioCVT(&converter,
-                wav_spec.format, wav_spec.channels, wav_spec.freq,
-                device_spec.format, device_spec.channels, device_spec.freq);
-
-        Buffer_Data buffer;
-
-        // NOTE(hugo): wav_spec is usable by the device_spec
-        if(converter_return_code == 0){
-            buffer.data = (u8*)malloc(wav_bytesize);
-            buffer.nsamples = wav_bytesize / sizeof(s16);
-
-            memcpy(buffer.data, wav_buffer, wav_bytesize);
-            SDL_FreeWAV(wav_buffer);
-
-        // NOTE(hugo): conversion is required
-        }else if(converter_return_code == 1){
-            converter.len = wav_bytesize;
-            converter.buf = (u8*)malloc(converter.len * converter.len_mult);
-
-            memcpy(converter.buf, wav_buffer, wav_bytesize);
-            SDL_FreeWAV(wav_buffer);
-
-            if(SDL_ConvertAudio(&converter)){
-                LOG_ERROR("SDL_ConvertAudio() FAILED - %s", SDL_GetError());
-            }
-
-            buffer.data = converter.buf;
-            buffer.nsamples = converter.len_cvt / sizeof(s16);
-
-        // NOTE(hugo): converter setup failed
-        }else if(converter_return_code < 0){
-            LOG_ERROR("SDL_BuildAudioCVT() FAILED - %s", SDL_GetError());
-        }
-
-        return {buffer_pool.insert(buffer)};
-    }
-    void unload_buffer(Audio_Buffer_ID buffer){
-        Buffer_Data& buffer_entry = buffer_pool[buffer];
-        ::free(buffer_entry.data);
-        buffer_pool.remove(buffer);
-    }
-
-    Audio_Playing_ID play_buffer(Audio_Buffer_ID buffer){
-        Play_Data play_data;
-        play_data.buffer = buffer;
-        play_data.cursor = 0u;
-        play_data.generation = manager_generation;
-
-        u32 index = play_pool.insert(play_data);
-        u32 generation = manager_generation;
-
-        ++manager_generation;
-
-        return {index, generation};
-    }
+    void mix_next_frame();
 
     // ---- data
 
+    SDL_AudioDeviceID device;
+    SDL_AudioSpec device_spec;
+
+    float* mix_buffer = nullptr;
     struct Audio_State{
-        u8* buffer = nullptr;
+        s16* buffer = nullptr;
         u32 nsamples = 0u;
 
         // NOTE(hugo): offset in samples
@@ -227,75 +40,23 @@ struct Audio_Manager{
         // generator_cursor:    read   by the audio thread, written by the generator thread
         volatile u32 reader_cursor = 0u;
         volatile u32 generator_cursor = 0u;
-    } callback_data;
-
-    SDL_AudioDeviceID device;
-    SDL_AudioSpec device_spec;
+    } state;
 
     struct Buffer_Data{
-        u8* data = nullptr;
+        s16* data = nullptr;
         u32 nsamples = 0u;
+        Buffer_Data* next = nullptr;
     };
-    dpool<Buffer_Data> buffer_pool;
+    dpool<Buffer_Data> buffers;
 
     struct Play_Data{
         Audio_Buffer_ID buffer;
         u32 cursor = 0u;
         u64 generation = 0u;
     };
-    dpool<Play_Data> play_pool;
-
     u64 manager_generation = 0u;
+    diterpool<Play_Data> to_play;
 };
-
-// NOTE(hugo): assumes that the generator will write with an offset of at least one frame in front of the audio thread
-static void audio_callback(void* user_ptr, u8* out_stream, s32 out_stream_size){
-    Audio_Manager::Audio_State* callback_data = (Audio_Manager::Audio_State*)user_ptr;
-    size_t sample_bytesize = sizeof(s16);
-
-    u32 reader_cursor = callback_data->reader_cursor;
-    u32 generator_cursor = atomic_get(&callback_data->generator_cursor);
-    u32 out_samples = (u32)out_stream_size / sample_bytesize;
-
-    // NOTE(hugo): handling the case where out_stream_size is not a multiple of sample_bytesize
-    u32 out_samples_bytesize = out_samples * (u32)sample_bytesize;
-    for(u32 izero_byte = (u32)out_stream_size - out_samples * sample_bytesize; izero_byte != (u32)out_stream_size; ++izero_byte){
-        out_stream[izero_byte] = 0;
-    }
-
-    // NOTE(hugo): single memcpy
-    if(generator_cursor > reader_cursor){
-        u32 samples_to_copy = min((generator_cursor - reader_cursor), out_samples);
-        u32 samples_to_zero = out_samples - samples_to_copy;
-        memcpy(out_stream, callback_data->buffer + reader_cursor * sample_bytesize, samples_to_copy * sample_bytesize);
-        memset(out_stream + samples_to_copy * sample_bytesize, 0, samples_to_zero * sample_bytesize);
-
-        atomic_set<u32>(&callback_data->reader_cursor, reader_cursor + samples_to_copy);
-
-    // NOTE(hugo): two memcpy and wrap around
-    }else if(generator_cursor < reader_cursor){
-        u32 samples_to_copy_end = min(callback_data->nsamples - reader_cursor, out_samples);
-        memcpy(out_stream, callback_data->buffer + reader_cursor * sample_bytesize, samples_to_copy_end * sample_bytesize);
-
-        u32 samples_copied = samples_to_copy_end;
-        if(samples_copied < out_samples){
-            u32 samples_to_copy_start = min(generator_cursor, out_samples - samples_copied);
-            memcpy(out_stream + samples_copied * sample_bytesize, callback_data->buffer, samples_to_copy_start * sample_bytesize);
-            samples_copied += samples_to_copy_start;
-            atomic_set<u32>(&callback_data->reader_cursor, samples_to_copy_start);
-        }else{
-            atomic_set<u32>(&callback_data->reader_cursor, reader_cursor + samples_to_copy_end);
-        }
-
-        u32 samples_to_zero = out_samples - samples_copied;
-        memset(out_stream + samples_copied * sample_bytesize, 0, samples_to_zero * sample_bytesize);
-
-    // NOTE(hugo): nothing provided - zero memory
-    }else{
-        //LOG_TRACE("!WARNING! nothing provided - audio dropout !WARNING!");
-        memset(out_stream, 0, out_samples * sample_bytesize);
-    }
-}
 
 // ---- hardware / software detection
 
@@ -303,3 +64,5 @@ void audio_detect_devices();
 void audio_detect_device_status(SDL_AudioDeviceID device);
 void audio_detect_drivers();
 void audio_detect_current_driver();
+
+#endif
