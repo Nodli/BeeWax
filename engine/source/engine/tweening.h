a84c1099da5ea7ecd9ab43996eaf47c7d3deb56e
Hugo Rambure <rambure.hugo@hotmail.fr>
2020-12-03 18:06:04 +0100

    modify tweening without manager

--- engine/source/engine/tweening.h
+++ engine/source/engine/tweening.h
@@ -1,126 +1,110 @@
 #ifndef H_TWEENING
 #define H_TWEENING
 
-#define FOR_EACH_TWEEN_TYPE(FUNCTION)   \
-FUNCTION(float)                         \
-FUNCTION(vec2)                          \
+// REF(hugo): https://easings.net/fr
+typedef float (*Tween_Function)(float t);
+
+float ease_none(float t);
+
+float ease_out_quint(float t);
+float ease_out_back(float t);
+
+template<Tween_Function>
+float yoyo(float t);
 
 template<typename T>
-struct Tween_ID{
-    explicit operator T() const {return *this;};
-    bool operator==(const Tween_ID<T>& rhs) const {return index == rhs.index && generation == rhs.generation;};
-    bool operator!=(const Tween_ID<T>& rhs) const {return !(*this == rhs);};
+struct Tween{
+    // NOTE(hugo): use as T
+    operator T () const;
+    Tween<T>& operator=(const Tween<T>& rhs);
 
-    u32 index;
-    u32 generation;
+    void set(const T& value);
+    void from(const T& start, const T& stop,
+            const u32 tick_duration, bool repeat = false,
+            Tween_Function func = &ease_out_quint);
+    void tick();
+
+    // ---- data
+
+    T value = {};
+    T base = {};
+    T range = {};
+    u32 tick_count = 0u;
+    s32 repeat_tick_duration = 1;
+    Tween_Function function = &ease_none;
 };
 
-template<typename T>
-constexpr Tween_ID<T> unknown_tween = {UINT_MAX, UINT_MAX};
+float ease_none(float t){
+    return t;
+}
 
-struct Tween_Manager{
-    template<typename T>
-    struct Tween{
-        T* ptr = nullptr;
-        u32 tick_count = 0u;
-        u32 tick_duration = 0u;
-        T base;
-        T range;
-        u32 generation;
-    };
+float ease_out_quint(float t){
+    float interm = 1.f - t;
+    float interm2 = interm * interm;
+    return 1.f - interm2 * interm2 * interm;
+}
 
-    void terminate();
+float ease_out_back(float t){
+    constexpr float c1 = 1.70158f;
+    constexpr float c3 = c1 + 1.f;
 
-    void next_tick();
+    float interm = t - 1.f;
+    float interm2 = interm * interm;
 
-#define DECLARE_TWEEN_METHODS(Type)                                                     \
-    Tween_ID<Type> start_tween(Type* ptr, Type start, Type stop, u32 tick_duration);    \
-    bool is_valid(Tween_ID<Type> tween);                                                \
-    void stop_tween(Tween_ID<Type> tween);
+    return 1.f + c3 * interm2 * interm + c1 * interm2;
+}
 
-    FOR_EACH_TWEEN_TYPE(DECLARE_TWEEN_METHODS)
-
-#undef DECLARE_TWEEN_METHODS
-
-    // --- data
-
-    u32 generation = 0u;
-
-#define DECLARE_TWEEN_STORAGE(Type) diterpool<Tween_Manager::Tween<Type>> CONCATENATE(storage_, Type);
-    FOR_EACH_TWEEN_TYPE(DECLARE_TWEEN_STORAGE)
-#undef DECLARE_TWEEN_STORAGE
-};
-
-void Tween_Manager::terminate(){
-#define TWEEN_TERMINATE(Type) CONCATENATE(storage_, Type).free();
-    FOR_EACH_TWEEN_TYPE(TWEEN_TERMINATE)
-#undef TWEEN_TERMINATE
+template<Tween_Function func>
+float yoyo(float t){
+    return func(abs(2.f * (t - 0.5f)));
 }
 
 template<typename T>
-static void storage_next_tick(diterpool<Tween_Manager::Tween<T>>& storage){
-    u32 index, counter;
-    for(index = storage.get_first(), counter = 0u;
-        index < storage.capacity;
-        index = storage.get_next(index), ++counter){
+Tween<T>::operator T () const{
+    return value;
+}
 
-        Tween_Manager::Tween<T>& tween = storage[index];
-        *tween.ptr = tween.base + tween.range * (float)tween.tick_count++ / (float)tween.tick_duration;
+template<typename T>
+Tween<T>& Tween<T>::operator=(const Tween<T>& rhs){
+    tick_count = rhs.tick_count;
+    repeat_tick_duration = rhs.tick_duration;
+    value = rhs.value;
+    base = rhs.base;
+    range = rhs.range;
+    return *this;
+}
 
-        if(tween.tick_count == tween.tick_duration){
-            storage.remove(index);
-        }
+template<typename T>
+void Tween<T>::set(const T& ivalue){
+    value = value;
+    base = value;
+    range = {};
+    tick_count = 1u;
+    repeat_tick_duration = 1u;
+    return *this;
+}
+
+template<typename T>
+void Tween<T>::from(const T& start, const T& stop, const u32 itick_duration, bool repeat, Tween_Function ifunc){
+    value = start;
+    base = start;
+    range = stop - start;
+    tick_count = 0u;
+    repeat_tick_duration = repeat ? (- itick_duration) : (itick_duration);
+    function = ifunc;
+}
+
+template<typename T>
+void Tween<T>::tick(){
+    bool repeat = repeat_tick_duration < 0;
+    u32 tick_duration = abs(repeat_tick_duration);
+
+    value = base + range * function((float)tick_count / (float)tick_duration);
+    tick_count = min(tick_count + 1u, tick_duration);
+
+    if(repeat && tick_count == tick_duration){
+        tick_count = 0u;
     }
 }
-
-void Tween_Manager::next_tick(){
-#define TWEEN_STORAGE_NEXT_TICK(Type) storage_next_tick(CONCATENATE(storage_, Type));
-    FOR_EACH_TWEEN_TYPE(TWEEN_STORAGE_NEXT_TICK)
-#undef TWEEN_STORAGE_NEXT_TICK
-}
-
-template<typename T>
-static Tween_ID<T> storage_start_tween(u32& manager_generation, diterpool<Tween_Manager::Tween<T>>& storage, T* ptr, T start, T stop, u32 tick_duration){
-    Tween_Manager::Tween<T> tween;
-    tween.ptr = ptr;
-    tween.tick_count = 0u;
-    tween.tick_duration = tick_duration;
-    tween.base = start;
-    tween.range = stop - start;
-    tween.generation = manager_generation++;
-
-    u32 tween_index = storage.insert(tween);
-    Tween_ID<T> output = {tween_index, tween.generation};
-    return output;
-}
-
-template<typename T>
-static bool storage_is_valid(diterpool<Tween_Manager::Tween<T>>& storage, Tween_ID<T> tween){
-    return tween.index < storage.capacity
-        && storage.is_active(tween.index)
-        && storage[tween.index].generation == tween.generation;
-}
-
-template<typename T>
-static void storage_stop_tween(diterpool<Tween_Manager::Tween<T>>& storage, Tween_ID<T> tween){
-    if(storage_is_valid(storage, tween)){
-        storage.remove(tween.index);
-    }
-}
-
-#define DEFINE_TWEEN_METHODS(Type)                                                                          \
-Tween_ID<Type> Tween_Manager::start_tween(Type* ptr, Type start, Type stop, u32 tick_duration){             \
-    return storage_start_tween(generation, CONCATENATE(storage_, Type), ptr, start, stop, tick_duration);   \
-}                                                                                                           \
-bool Tween_Manager::is_valid(Tween_ID<Type> tween){                                                         \
-    return storage_is_valid(CONCATENATE(storage_, Type), tween);                                            \
-}                                                                                                           \
-void Tween_Manager::stop_tween(Tween_ID<Type> tween){                                                       \
-    storage_stop_tween(CONCATENATE(storage_, Type), tween);                                                 \
-}
-
-    FOR_EACH_TWEEN_TYPE(DEFINE_TWEEN_METHODS)
-
-#undef DEFINE_TWEEN_METHODS
 
 #endif
