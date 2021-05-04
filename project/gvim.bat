218a3b787274753b6dbc61070b2fdaef82cef89c
Hugo Rambure <rambure.hugo@hotmail.fr>
2021-05-04 18:26:59 +0200

    fix gvim.bat

--- project/gvim.bat
+++ project/gvim.bat
@@ -1,12 +1,12 @@
 @echo off
 
-pushd %cd%
+pushd %~dp0
 
 set session_path=%cd%\session.vim
 set gvim_path="C:\Program Files (x86)\Vim\vim82\gvim.exe"
 
 start /MAX "" /D "." %gvim_path%                                                                                                                                                      	        ^
-     -c ":autocmd VimEnter * if filereadable(expand(\"%session_path%\")) | :source %session_path% | else | :args **\*.h **\*.cpp **\*.inl **\*.txt | :set filetype=cpp | :b todo.txt | endif"   ^
+     -c ":autocmd VimEnter * if filereadable(expand(\"%session_path%\")) | :source %session_path% | else | :args source/*.* | :set filetype=cpp | :e todo.txt | | endif"   ^
      -c ":autocmd VimLeave * mksession! %session_path%"
 
 popd
